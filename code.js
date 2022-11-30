4b048bff0b0e736326898a37beeb1cea22780862
Tyler Whitman <2tylerwhitman4@gmail.com>
2022-11-30 15:15:08 -0600

    exercise 1

--- code.js
+++ code.js
@@ -1,4 +1,8 @@
-// helloWorld function 
+// helloWorld function
 function helloWorld() {
     return "Hello, World!";
+}
+
+function sayHello(){
+
 }
--- tests.js
+++ tests.js
@@ -13,3 +13,9 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello', function(){
+    it('should be a defined function', function(){
+        expect(sayHello()).toBe("string");
+    })
+})
